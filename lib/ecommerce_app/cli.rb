5ebbdd6a1bc03008e4199af49c87e6360ca2aab6
chienleow <chienleow1@gmail.com>
2020-04-28 22:21:30 -0400

    commit.

--- lib/ecommerce_app/cli.rb
+++ lib/ecommerce_app/cli.rb
@@ -3,14 +3,31 @@ class EcommerceApp::CLI
     def start
         puts "Welcome to The eCommerce Scouting App!"
         EcommerceApp::API.get_category
-        category_number
+        category_list
     end
 
-    def category_number
-        puts "Please enter the category number you want to search for:"
+    def category_list
+        ## a list of category needs to print out here
+        puts "\nPlease enter the category number you want to search for:"
         input = gets.strip
+        ## the number list if else way of doing it
+        else
+            invalid_input
+        end
     end
 
-    def validate_input(input)
+    def invalid_input(input)
+        puts "\nSorry, input error, please try again."
+        category_list
     end
+
+    def selected_category(category)
+        puts "Here are the listings and their prices in #{category}."
+    end
+
+    def goodbye
+        "Goodbye, thanks for using the app, happy selling!"
+    end
+
+
 end
