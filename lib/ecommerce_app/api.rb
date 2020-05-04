8501192f70e88bbcce0ed98ee0243fae6091b7c9
chienleow <chienleow1@gmail.com>
2020-05-03 23:05:38 -0400

    commit.

--- lib/ecommerce_app/api.rb
+++ lib/ecommerce_app/api.rb
@@ -1,7 +1,7 @@
 class EcommerceApp::API
     def self.get_category
-        info = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key=znxfsfsmw75wblw4b3fyre34")
-        binding.pry
+        
+        # binding.pry
     end
 
     def self.get_listing
