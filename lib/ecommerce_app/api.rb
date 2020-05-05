dd79db9c619824bee6a5dcc180367026c76f3b4d
chienleow <chienleow1@gmail.com>
2020-05-04 21:52:34 -0400

    commit.

--- lib/ecommerce_app/api.rb
+++ lib/ecommerce_app/api.rb
@@ -1,11 +1,29 @@
 class EcommerceApp::API
     def self.get_category
-        info = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key=#{ENV['ETSY_API_KEY']}")
+        # info = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key=#{ENV['ETSY_API_KEY']}")
+        response = HTTParty.get("http://open.api.ebay.com/Shopping?callname=GetCategoryInfo&
+        appid=#{ENV['EBAY_API_KEY']}&siteid=3&CategoryID=-1&version=729&
+        IncludeSelector=ChildCategories")
         binding.pry
     end
 
+    #display index number right next to each category name and category ID
+    #user choose from index number
+    #when user enter index number, connect that index number to category ID
+    #user category ID for next search
+
+
+    def category_id
+    end
+
     def self.get_listing
-        ##listings with their titles and prices
+        response_2 = HTTPaarty.get("https://svcs.ebay.com/services/search/FindingService/v1?
+        OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&
+        SECURITY-APPNAME=#{ENV['EBAY_API_KEY']}&
+        RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&
+        categoryId=#{category_id}&paginationInput.entriesPerPage=10")
+        # binding.pry
+
         
     end
 
