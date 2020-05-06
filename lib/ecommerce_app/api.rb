ec6104c73b080d2331cfcb8d12309f4be107537a
chienleow <chienleow1@gmail.com>
2020-05-05 23:18:34 -0400

    commit.

--- lib/ecommerce_app/api.rb
+++ lib/ecommerce_app/api.rb
@@ -1,27 +1,22 @@
 class EcommerceApp::API
     def self.get_category
-        # info = HTTParty.get("https://openapi.etsy.com/v2/listings/active?api_key=#{ENV['ETSY_API_KEY']}")
-        response = HTTParty.get("http://open.api.ebay.com/Shopping?callname=GetCategoryInfo&
-        appid=#{ENV['EBAY_API_KEY']}&siteid=3&CategoryID=-1&version=729&
-        IncludeSelector=ChildCategories")
-        binding.pry
+        response = HTTParty.get("http://open.api.ebay.com/Shopping?callname=GetCategoryInfo&appid=#{ENV['EBAY_API_KEY']}&siteid=0&CategoryID=-1&version=1141&IncludeSelector=ChildCategories")
+        response["GetCategoryInfoResponse"]["CategoryArray"]["Category"].each.with_index(1) do |item, index|
+            puts "#{index}. #{item["CategoryName"]}, #{item["CategoryID"]}"
+        end
+        response
     end
 
-    #display index number right next to each category name and category ID
-    #user choose from index number
-    #when user enter index number, connect that index number to category ID
-    #user category ID for next search
+#   to_do:
+#   1. remove first "Root, -1"
 
 
     def category_id
+
     end
 
     def self.get_listing
-        response_2 = HTTPaarty.get("https://svcs.ebay.com/services/search/FindingService/v1?
-        OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&
-        SECURITY-APPNAME=#{ENV['EBAY_API_KEY']}&
-        RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&
-        categoryId=#{category_id}&paginationInput.entriesPerPage=10")
+        response_2 = HTTPaarty.get("https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=#{ENV['EBAY_API_KEY']}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&categoryId=#{category_id}&paginationInput.entriesPerPage=10")
         # binding.pry
 
         
@@ -32,5 +27,10 @@ class EcommerceApp::API
 end
 
 
-
-
+# response["GetCategoryInfoResponse"].each do |category_array|
+# category_array.each do |category|
+# category.each do |key, value|
+#     puts "#{key} = #{value}"
+# end
+# end
+# end
