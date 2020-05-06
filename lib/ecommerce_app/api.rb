ca593846d625511dd677caf0b116d073ef6863c3
chienleow <chienleow1@gmail.com>
2020-05-06 16:22:01 -0400

    commit.

--- lib/ecommerce_app/api.rb
+++ lib/ecommerce_app/api.rb
@@ -16,6 +16,7 @@ class EcommerceApp::API
         response_2 = HTTParty.get("https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=#{ENV['EBAY_API_KEY']}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&categoryId=#{category_id}&paginationInput.entriesPerPage=10")
         response_2["findItemsByCategoryResponse"]["searchResult"]["item"].each.with_index(1) do |item, index|
             puts "#{index}. #{item["title"]}"
+            # binding.pry
         end
     end
 
